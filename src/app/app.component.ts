012e503549f1d2590e6f027a0ac6d50c11ac1056
esther <ecmango24@gmail.com>
2019-07-02 17:29:14 -0700

    added routing for home and session buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+//import { HomeComponent } from 'home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+    <router-outlet></router-outlet>
+  `
+  //'./app.component.html',
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Beacon-Test-App-V1';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SessionComponent } from './session/session.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'app-session', component: SessionComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/app-session'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
